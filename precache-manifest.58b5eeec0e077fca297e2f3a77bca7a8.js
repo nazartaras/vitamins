32647bf6ad205b68aa3ef630258b2579716cbbff
Taras Nazar <32059567+nazartaras@users.noreply.github.com>
2019-09-28 00:43:56 +0300

    Updates

---
+++ precache-manifest.58b5eeec0e077fca297e2f3a77bca7a8.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f713e8d697d9d9ad90c70f7ebc7ec66",
+    "url": "/vitamins/index.html"
+  },
+  {
+    "revision": "7704b33efde95864afdc",
+    "url": "/vitamins/static/css/2.db06e11c.chunk.css"
+  },
+  {
+    "revision": "023d59f4a8a642b1baa7",
+    "url": "/vitamins/static/css/main.efb21908.chunk.css"
+  },
+  {
+    "revision": "7704b33efde95864afdc",
+    "url": "/vitamins/static/js/2.2eaf8210.chunk.js"
+  },
+  {
+    "revision": "023d59f4a8a642b1baa7",
+    "url": "/vitamins/static/js/main.586bb8e1.chunk.js"
+  },
+  {
+    "revision": "563f49fd5b9086337200",
+    "url": "/vitamins/static/js/runtime-main.22b5c7da.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/vitamins/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/vitamins/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/vitamins/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/vitamins/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/vitamins/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/vitamins/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/vitamins/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/vitamins/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/vitamins/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/vitamins/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/vitamins/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/vitamins/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/vitamins/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/vitamins/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/vitamins/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/vitamins/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
